55650d8bf937a8e6e6973f98e1d85e86457cf32d
juliomadjan <92974420+juliomadjan@users.noreply.github.com>
2021-10-29 16:46:15 +0700

    Dictionary & Function

---
+++ Pertemuan-4.py
@@ -0,0 +1,32 @@
+# # DICTIONARY
+# pelanggan = {
+#     "nama" : "julio",
+#     "umur" : 23
+# }
+
+# pelanggan_2 = {
+#     "nama" : "aji",
+#     "umur" : 28
+# }
+
+
+# print("Nama : {}".format(pelanggan.["nama"]))
+
+# for x in pelanggan:
+#     print(x)
+#     print(pelanggan[x])
+#     print(pelanggan_2[x])
+
+
+# FUNCTION
+def myfunction(first_name, last_name):
+    print("Hello {} {}".format(first_name, last_name))
+
+myfunction("Julio", "Madjan")
+# Bisa dibalik inputannya
+myfunction(last_name = "Madjan", first_name = "Julio")
+
+
+nama_buah = ["apel","jeruk","mangga","delima","duku"]
+
+for buah in ["manggis, durian"]:
