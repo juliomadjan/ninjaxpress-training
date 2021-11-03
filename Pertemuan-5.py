9dedc0f1839219d81819d0c3920891a7047d3ca8
juliomadjan <92974420+juliomadjan@users.noreply.github.com>
2021-11-03 15:24:27 +0700

    Modules & File Handling

---
+++ Pertemuan-5.py
@@ -0,0 +1,39 @@
+# import mymodule
+
+# mymodule.greeting("Julio")
+# mymodule.goodbye("Julio")
+
+# import math
+# import datetime
+
+# x = math.pi 
+# print(x)
+
+# x = math.sqrt(64)
+# print(x)
+
+# x = math.sin(math.degrees(30))
+# print(x)
+
+# print(datetime.datetime.now())
+# print(datetime.date.today())
+
+# #FILE HANDLING
+# f = open("file.txt", "w")
+# f.write("Added text\n")
+# f.close()
+
+# f = open("file.txt", "a")
+# f.write("Hello!\n")
+# f.close()
+
+# f = open("file.txt", "r")
+# print(f.read())
+
+# f = open("file.txt", "r")
+# print(f.read(5))
+
+# import matematika
+
+# print("Pi: ", matematika.pi)
+# print("Luas: ",matematika.luas_persegi(sisi=12))
